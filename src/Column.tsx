8fa1cefdf6ed8614442925dca119458ca969ecab
Muhammad Meganata Adam <bangadam.dev@gmail.com>
2024-12-10 22:43:50 +0700

    initialize project with Vite, React, and TypeScript setup

---
+++ src/Column.tsx
@@ -0,0 +1,57 @@
+import { ColumnContainer, ColumnTitle } from "./styles";
+import { Card } from "./Card";
+import { AddNewItem } from "./AddNewItem";
+import { useAppState } from "./state/AppStateContext";
+import { addTask, moveList } from "./state/actions";
+import { useRef } from "react";
+import { useItemDrag } from "./utils/useItemDrag";
+import { useDrop } from "react-dnd";
+import { throttle } from "throttle-debounce-ts";
+import { isHidden } from "./utils/isHidden";
+
+type ColumnProps = {
+	text: string;
+	id: string;
+	isPreview?: boolean;
+};
+
+export const Column = ({ text, id, isPreview }: ColumnProps) => {
+	const { draggedItem, getTasksByListId, dispatch } = useAppState();
+	const tasks = getTasksByListId(id);
+	const ref = useRef<HTMLDivElement>(null);
+	const { drag } = useItemDrag({ type: "COLUMN", id, text });
+	const [, drop] = useDrop({
+		accept: "COLUMN",
+		hover: throttle(200, () => {
+			if (!draggedItem) {
+				return;
+			}
+			if (draggedItem.type === "COLUMN") {
+				if (draggedItem.id === id) {
+					return;
+				}
+				dispatch(moveList(draggedItem.id, id));
+			}
+		}),
+	});
+
+	drag(drop(ref));
+
+	return (
+		<ColumnContainer
+			ref={ref}
+			isHidden={isHidden(draggedItem, "COLUMN", id, isPreview)}
+			isPreview={isPreview}
+		>
+			<ColumnTitle>{text}</ColumnTitle>
+			{tasks.map((task) => (
+				<Card text={task.text} key={task.id} id={task.id} columnId={id} />
+			))}
+			<AddNewItem
+				toggleButtonText="+ Add another card"
+				onAdd={(text) => dispatch(addTask(text, id))}
+				dark
+			/>
+		</ColumnContainer>
+	);
+};
